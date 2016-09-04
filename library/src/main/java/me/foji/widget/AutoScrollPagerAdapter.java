28d1f187b8ea46d167758b47726e70fe4e418a4a
scott <626306805@qq.com>
2016-09-04 11:50:21 +0800

    删除冗余代码

--- library/src/main/java/me/foji/widget/AutoScrollPagerAdapter.java
+++ library/src/main/java/me/foji/widget/AutoScrollPagerAdapter.java
@@ -8,12 +8,7 @@ import java.util.ArrayList;
  * @author Scott Smith  @Date 2016年08月16/8/30日 19:51
  */
 public abstract class AutoScrollPagerAdapter extends IBindView {
-    private OnChangeListener onChangeListener;
     private ArrayList<OnChangeListener> listeners = new ArrayList<>();
-
-    public void setOnChangeLister(OnChangeListener onChangeListener) {
-        this.onChangeListener = onChangeListener;
-    }
 
     public void addOnChangeListener(OnChangeListener listener) {
         listeners.add(listener);
@@ -21,10 +16,9 @@ public abstract class AutoScrollPagerAdapter extends IBindView {
 
     public void notifyDataSetChanged() {
         synchronized(this) {
-           if(null != onChangeListener) onChangeListener.onChange();
-        }
-        for(OnChangeListener listener : listeners) {
-            listener.onChange();
+            for(OnChangeListener listener : listeners) {
+                listener.onChange();
+            }
         }
     }
 }
